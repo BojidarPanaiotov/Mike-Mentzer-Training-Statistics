34ae2f7c876b1342e44b1590d4b26b4b9fe7ec8f
Bozhidar Panayotov <bozhidar.panayotov@c4nexus.com>
2023-06-27 11:43:57 +0300

    Introduce bootstrap

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const express = require('express');
 const app = express();
 
 app.use(express.static('public'));
+app.use('/css',express.static('node_modules/bootstrap/dist/css'));
+app.use('/js',express.static('node_modules/bootstrap/dist/js'));
 
 app.set('view engine', 'ejs');
 
