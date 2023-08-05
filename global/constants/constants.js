68ff02d4399fca7de22f0980e996da5851b6f1ea
Bozhidar Panayotov <62803702+BojidarPanaiotov@users.noreply.github.com>
2023-08-05 13:16:53 +0300

    Sync data and add two more exercise

--- global/constants/constants.js
+++ global/constants/constants.js
@@ -3,9 +3,9 @@ const TOP_EXERCISE_COUNT = 5;
 const DATABASE_PATH = 'data/workouts.json';
 const ALL_EXERCISES = {
   WORKOUT_TYPES: ['Chest & Back', 'Legs & Abs', 'Shoulders & Arms'],
-  PUSH: ['Dumbbell Lateral Raises', 'Machine Lateral Deltoids', 'Triceps Cable', 'Dumbbell Reverse Flyes', 'Machine Peck Back Shoulder', 'Machine Chest Flyes', 'Upper Bench Press'],
+  PUSH: ['Dumbbell Lateral Raises', 'Machine Lateral Deltoids', 'Triceps Cable', 'Dumbbell Reverse Flyes', 'Machine Peck Back Shoulder', 'Machine Chest Flyes', 'Upper Bench Press', 'Dips'],
   PULL: ['Pulldown Bicep Curl', 'Pull Ups', 'Dead Lift', 'Lat Pull Down Cable', 'Preacher Curl Straight Bar'],
-  LEGS: ['Leg Press', 'Leg Extension', 'Calf Rise', 'Seated Calf Rise', 'Adductor Machine'],
+  LEGS: ['Leg Press', 'Leg Extension', 'Calf Rise', 'Seated Calf Rise', 'Adductor Machine', 'Leg Curl'],
   ABS: ['Sit Ups']
 };
 
